bd13cd53dd5a8ef08c135e7f8668e790c839d531
Roman Prokopchuk <ghost62727@gmail.com>
2026-03-10 13:23:52 +0200

    Init project

---
+++ src/main/java/org/prokopchuk/chemistry_calculator/ChemistryCalculatorApplication.java
@@ -0,0 +1,13 @@
+package org.prokopchuk.chemistry_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemistryCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChemistryCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/prokopchuk/chemistry_calculator/ChemistryCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prokopchuk.chemistry_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemistryCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
